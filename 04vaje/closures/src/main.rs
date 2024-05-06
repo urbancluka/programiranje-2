35ff6fe4fc099d09fcb7a3c1fc6ada8bd0d6226b
j00sko <koprivec.filip@gmail.com>
2024-05-06 12:01:43 +0200

    Vaje iteratorji

--- 04vaje/closures/src/main.rs
+++ 04vaje/closures/src/main.rs
@@ -9,3 +9,75 @@
 //  filter: ('a -> bool) -> 'a list -> 'a list // Vrne seznam elementov, ki zadoščajo pogoju - uporabite Vec<T> namesto list in že vgrajeno funkcijo filter
 
 // Vzemite zaporedja iz prejšnjih vaj in naredite nov objekt, ki sprejme zaporedje in ga naredi iterabilnega
+
+// Iteratorji
+
+// Napišite funkcijo, ki sprejme vektor XYZ in s pomočjo iteratorja naredi W
+// števil in izpiše vsako v svojo vrstico
+// nizov in izpiše njihove dolžine
+// nizov in vrne vsoto njihovih dolžin
+// vektor parov (i32, i32) in vrne vsoto njihovih pozitivnih produktov
+// dva vektorja <i32> in vrne vektor, ki vsebuje vsote parov
+// dva vektorja <i32> in vrne vsoto poparjenih pozitivni produktov s pomočjo ene izmed prejšnjih nalog
+// vektor Option<T> in izpiše vse T-je
+// vektor Option<T> in vrne število Some-ov
+// odfiltrira števila deljena s 3
+
+// Dopolnite spodnjo funkcijo, da vrne niz, kjer so vse prve črke posameznih besed velike
+// ["Just,", " ", "hello", " ", "world", "!"] -> "Just, Hello World", "!"
+pub fn capitalize_words_string(words: &[&str]) -> String {
+    panic!("Not implemented");
+}
+// Napišite funkcijo `fakulteta`, ki izračuna fakulteto števila n. Uporabite iteratorje (torej brez lastne for zanke, rekurzije)
+// Namig: fold, reduce, `..`...
+
+// -------------------------------------------------------------------------------------------------
+/// Dodatno:
+// Koda vzeta iz googlvih rust vaj:
+// Vse se da lepo narediti samo z iteratorji (brez indeksov, brez for zank, brez mutabilnosti)
+/*
+/// Calculate the differences between elements of `values` offset by `offset`,
+/// wrapping around from the end of `values` to the beginning.
+///
+/// Element `n` of the result is `values[(n+offset)%len] - values[n]`.
+fn offset_differences<N>(offset: usize, values: Vec<N>) -> Vec<N>
+where
+    N: Copy + std::ops::Sub<Output = N>,
+{
+    unimplemented!()
+}
+
+#[test]
+fn test_offset_one() {
+    assert_eq!(offset_differences(1, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+    assert_eq!(offset_differences(1, vec![1, 3, 5]), vec![2, 2, -4]);
+    assert_eq!(offset_differences(1, vec![1, 3]), vec![2, -2]);
+}
+
+#[test]
+fn test_larger_offsets() {
+    assert_eq!(offset_differences(2, vec![1, 3, 5, 7]), vec![4, 4, -4, -4]);
+    assert_eq!(offset_differences(3, vec![1, 3, 5, 7]), vec![6, -2, -2, -2]);
+    assert_eq!(offset_differences(4, vec![1, 3, 5, 7]), vec![0, 0, 0, 0]);
+    assert_eq!(offset_differences(5, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+}
+
+#[test]
+fn test_custom_type() {
+    assert_eq!(
+        offset_differences(1, vec![1.0, 11.0, 5.0, 0.0]),
+        vec![10.0, -6.0, -5.0, 1.0]
+    );
+}
+
+#[test]
+fn test_degenerate_cases() {
+    assert_eq!(offset_differences(1, vec![0]), vec![0]);
+    assert_eq!(offset_differences(1, vec![1]), vec![0]);
+    let empty: Vec<i32> = vec![];
+    assert_eq!(offset_differences(1, empty), vec![]);
+}
+
+
+
+*/
